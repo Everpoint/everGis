8ac28d62639512ee9bbe5f88cf888d6cd0c9c428
mg <mg@everpoint.ru>
2015-05-22 17:12:04 +0300

    Added map tip module

---
+++ Source/spatialProcessor/MapTip.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function() {
+
+    sGis.spatialProcessor.MapTip = function(connector) {
+        this._connector = connector;
+        this._url = connector.url + 'api/maxtip/';
+    };
+
+    sGis.utils.proto.setMethods(sGis.spatialProcessor.MapTip.prototype, {
+        getUrl: function(storageId, objectId) {
+            return this._url + 'build?storageId=' + storageId + '&id=' + objectId + '&sb=' + this._connector.sessionId;
+        }
+    });
+
+})();
