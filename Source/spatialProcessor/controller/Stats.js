822f1f9bc565d05605312b92920bb10dad52014a
Maximkaaa <maxim@gritsenko.biz>
2015-09-07 17:28:31 +0300

    ED-1443 .highlight method for statistics

--- Source/spatialProcessor/controller/Stats.js
+++ Source/spatialProcessor/controller/Stats.js
@@ -4,6 +4,7 @@
 
     sGis.spatialProcessor.controller.Stats = function(spatialProcessor, options) {
         this.__initialize(spatialProcessor, {sync: true}, function() {
+            this._layer = new sGis.spatialProcessor.MapServer('VisualObjectsRendering/' + this._mapServiceId, this._spatialProcessor, {map: options.map, display: this._display});
             this.initialized = true;
             this.fire('initialize');
         });
@@ -20,6 +21,18 @@
                 return {
                     operation: 'build',
                     dataParameters: param,
+                    success: properties.success,
+                    error: properties.error,
+                    requested: properties.requested
+                };
+            });
+        },
+
+        highlight: function(properties) {
+            this.__operation(function() {
+                return {
+                    operation: 'maxlight',
+                    dataParameters: 'highlightId=' + properties.highlightId,
                     success: properties.success,
                     error: properties.error,
                     requested: properties.requested
