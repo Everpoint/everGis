400ba29083f5e553576a42692bae4b3a17888c18
Maximkaaa <maxim@gritsenko.biz>
2015-09-30 13:45:35 +0300

    Fixed the scale lock for dynamic services

--- Source/spatialProcessor/mapItem/DynamicServiceLayer.js
+++ Source/spatialProcessor/mapItem/DynamicServiceLayer.js
@@ -108,7 +108,7 @@
                 var resolution = this.map.resolution;
                 if (this._minScale !== 0 || this._maxScale !== 0) {
                     var scale = resolution * this._dpm;
-                    if (this._maxScale && (scale > this._maxScale) || this._minScale && (scale < this._minScale)) return false;
+                    if (scale < this._maxScale || this._minScale && scale > this._minScale) return false;
                 }
 
                 var limits = this.resolutionLimits;
