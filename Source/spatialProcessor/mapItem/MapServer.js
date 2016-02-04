baad5131a994a0babd706e8e0de88d4f5f2710ce
Maximkaaa <maxim@gritsenko.biz>
2016-02-04 09:00:00 +0300

    ED-1783 Fixed loading of resolution limits for services

--- Source/spatialProcessor/mapItem/MapServer.js
+++ Source/spatialProcessor/mapItem/MapServer.js
@@ -70,6 +70,8 @@
                 }
             }
 
+            if (this._resolutionLimits) this.resolutionLimits = this._resolutionLimits;
+
             this.fire('propertyChange', {property: 'name'});
         },
 
