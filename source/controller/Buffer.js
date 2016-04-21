ae7f5b9d1c33a378c150e014695e12d8a7fa5552
Maxim Gritsenko <maxim@gritsenko.biz>
2016-04-21 14:57:33 +0900

    ED-1831 Added subtractInnerBuffer parameter to the .calculateBuffers() method

--- source/controller/Buffer.js
+++ source/controller/Buffer.js
@@ -21,11 +21,12 @@ sGis.module('spatialProcessor.controller.Buffer', [
          * Calculates the buffers and displays them in the controller mapServer
          * @param {Object} properties
          * @param {Number|String[]} properties.distances - an array of buffer radiuses or attribute names, which should be used as a value for radius.
-         * @param {Boolean} properties.unionResults
-         * @param {String} properties.storageId
-         * @param {Boolean} properties.subtractObject
+         * @param {Boolean} properties.unionResults - whether to unite buffers into one object.
+         * @param {String} properties.storageId - id of the server storage with target geometries
+         * @param {Boolean} properties.subtractObject - whether to subtract the source object from the resulting buffer.
          * @param {Number} [properties.processDelay] - server processes objects in batches of 200. This parameter is a sleep time in ms between batches. Use smaller value for quicker process.
-         * @param {sGis.Crs} properties.sr
+         * @param {sGis.Crs} properties.sr - spatial reference of the source geometry. If not specified, will be used spatial reference of the target layer.
+         * @param {Boolean} properties.subtractInnerBuffer - whether to subtract inner buffer from outer buffer. This option has no effect if "unionResult" is true.
          * @param {Function} properties.requested
          * @param {Function} properties.success
          * @param {Function} properties.error
@@ -38,6 +39,7 @@ sGis.module('spatialProcessor.controller.Buffer', [
                 params += '&subtractObject=' + (properties.subtractObject || false);
                 if (properties.processDelay) params += '&processDelay=' + properties.processDelay;
                 if (properties.sr) params += '&sr=' + JSON.stringify(properties.sr.getWkidString());
+                if (properties.subtractInnerBuffer) params += '&subtractInnerBuffer=' + properties.subtractInnerBuffer;
 
                 params += '&autoUpdate=false';
                 return {
