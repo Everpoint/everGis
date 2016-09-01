6a1ac3b1c6b99aff774c148afb25bb073ecdae6e
TPoryadin <tporyadin@everpoint.ru>
2016-09-01 16:16:50 +0300

    ED-2030 LayerManager

--- source/LayerManager.js
+++ source/LayerManager.js
@@ -4,104 +4,16 @@
 sGis.module('spatialProcessor.LayerManager', [
     'utils',
     'IEventHandler',
+    'spatialProcessor.OrderManager',
     'spatialProcessor.MapService',
     'spatialProcessor.mapService.TileService',
     'LayerGroup'
-], function (utils, IEventHandler, MapService, TileService, LayerGroup) {
+], function (utils, IEventHandler, OrderManager, MapService, TileService, LayerGroup) {
+
+    const ActiveBasemapSymbol = Symbol("Basemap");
 
     /**
-     * Class for managing order of async loaded objects array
-     */
-    class OrderManager {
-        /**
-         * @constructor
-         * @param {Array} [ids=[]] - ordered array of unique ids.
-         */
-        constructor(ids=[]){
-            this._ids = ids;
-            this._currIds = [];
-        }
-
-        /**
-         * Order list of ids
-         * @returns {Array}
-         */
-        get ids () {
-            return this._ids;
-        }
-
-        /**
-         * Order list of  loaded ids
-         * @returns {Array}
-         */
-        get currIds () {
-            return this._currIds;
-        }
-
-        /**
-         * Return index of object
-         * @param id - unique id of object
-         * @returns {number} objects index
-         */
-        getIndex (id) {
-            const index = this._ids.indexOf(id);
-            return index > 0? index : (this._ids.push(id)-1)
-        }
-
-        /**
-         * Return current index of object
-         * @param index
-         * @param id
-         * @returns {number} current objects index
-         */
-        getCurrentIndex (index, id) {
-            this._currIds[index] = id;
-            return this._currIds.filter(id => !!id).indexOf(id);
-        }
-
-        /**
-         * Move id in ids array
-         * @param id id
-         * @param direction direction
-         * @returns {number} new ids index
-         */
-        moveId (id, direction) {
-            const {ids, currIds} = this;
-            const currIndex = ids.indexOf(id);
-            const newIndex = currIndex + direction;
-            const movedId = ids[newIndex];
-
-            if(newIndex < 0 || newIndex > ids.length) {
-                return currIndex;
-            }
-
-            ids[currIndex] = movedId;
-            ids[newIndex] = id;
-            currIds[currIndex] = movedId;
-            currIds[newIndex] = id;
-
-            return newIndex;
-        }
-
-        /**
-         * Remove id from ids array
-         * @param id id
-         * @returns {number} index of removed id
-         */
-        removeId (id) {
-            const {ids, currIds} = this;
-            const index = ids.indexOf(id);
-
-            this._ids = [...ids.slice(0, index), ...ids.slice(index+1)];
-            this._currIds = [...currIds.slice(0, index), ...currIds.slice(index+1)];
-
-            return index;
-        }
-
-    }
-
-    /**
-     * @alias sGis.spatialProcessor.LayerController
+     * @alias sGis.spatialProcessor.LayerManager
      */
     class LayerManager {
         /**
@@ -109,7 +21,9 @@ sGis.module('spatialProcessor.LayerManager', [
          * @returns {Array.<Object>} Ordered array of init services
          */
         get services() {
-            return this._layers.ids.map(id=>this._services[id]).filter(service=>!!service);
+            return [this._services[ActiveBasemapSymbol]].concat(
+                this._layers.ids.map(id=>this._services[id])
+            ).filter(service=>!!service);
         }
 
         /**
@@ -118,10 +32,11 @@ sGis.module('spatialProcessor.LayerManager', [
          * @param {Object} api
          * @param {Object} connector
          */
-        constructor (map, api, connector) {
+        constructor (connector, map, api, painter) {
             this._map = map;
             this._api = api;
             this._connector = connector;
+            this._painter = painter;
             this._layers = new OrderManager();
             this._services = {};
         }
@@ -131,8 +46,9 @@ sGis.module('spatialProcessor.LayerManager', [
          * @param services {Array} array of service names from settings
          */
         init (services) {
-            //this._baseMapControl = new sGis.controls.BaseLayerSwitch(this.painter);
+            this._basemapGroup = new LayerGroup();
             this._layerGroup = new LayerGroup();
+            this._map.addLayer(this._basemapGroup);
             this._map.addLayer(this._layerGroup);
 
             this.loadFromSettings(services);
@@ -141,43 +57,45 @@ sGis.module('spatialProcessor.LayerManager', [
 
         loadFromSettings (services) {
             services.forEach(name=>{
-                this.addService(name);
+                this.loadService(name);
             });
         }
 
         loadBasemapList () {
             return this._api.getServiceCatalog({
-                filter: [{"PropertyPath":["Basemap"], "Value":true}],
+                jsfilter: [{"PropertyPath":["Basemap"], "Value":true}],
             }).then(basemaps=>{
+                this.fire("loadBasemaps", { basemaps });
                 return basemaps;
             }).catch(message => {
-                utils.message(message);
+                utils.error(message);
             });
         }
 
-        addService (name) {
-            const realIndex = this._layers.getIndex(name);
-
+        loadService (name) {
             MapService.initialize(this._connector, name)
             .then(service => {
                 if (service.layer) {
                     if (service instanceof TileService) {
-                        this._map.crs = service.layer.crs;
-                        this._map.tileScheme = service.layer.tileScheme;
-                        this._map.adjustResolution();
+                        this.addBasemap(service);
+                    } else {
+                        const realIndex = this._layers.getIndex(name);
+                        this.addService(service, realIndex);
                     }
-
-                    const index = this._layers.getCurrentIndex(realIndex, name);
-                    this._services[name] = service;
-
-                    this._layerGroup.insertLayer(service.layer, index);
-
-                    this.fire('serviceAdd', {service, index: realIndex});
                 }
             })
             .catch(message => {
                 utils.message(message);
             });
+        }
+
+        addService (service, realIndex) {
+            const index = this._layers.getCurrentIndex(realIndex, name);
+            this._services[service.name] = service;
+
+            this._layerGroup.insertLayer(service.layer, index);
+
+            this.fire('serviceAdd', {service, index: realIndex});
         }
 
         removeService (name) {
@@ -203,12 +121,47 @@ sGis.module('spatialProcessor.LayerManager', [
         }
 
         toggleService (name) {
-            const {isDisplayed} = this._services[name];
-            this.fire('serviceToggle', {service: this._services[name]});
-            return this._services[name].isDisplayed = isDisplayed !== true;
+            if (this._services[name]) {
+                const {isDisplayed} = this._services[name];
+                this.fire('serviceToggle', {service: this._services[name]});
+                return this._services[name].isDisplayed = isDisplayed !== true;
+            } else if (this._services[ActiveBasemapSymbol] &&
+                this._services[ActiveBasemapSymbol].name === name) {
+                return this.toggleBasemap(this._services[ActiveBasemapSymbol]);
+            }
         }
 
-        getActiveServiceList () {
+        addBasemap (basemap) {
+            if (!this._services[ActiveBasemapSymbol]) {
+                this._map.crs = basemap.layer.crs;
+                this._map.tileScheme = basemap.layer.tileScheme;
+                this._map.adjustResolution();
+            } else {
+                this._basemapGroup.removeLayer(this._services[ActiveBasemapSymbol].layer);
+            }
+            this._services[ActiveBasemapSymbol] = basemap;
+            this._basemapGroup.insertLayer(basemap.layer, 0);
+            this.fire('serviceAdd', { basemap });
+        }
+
+        toggleBasemap (basemap) {
+            return basemap.isDisplayed = basemap.isDisplayed !== true;
+        }
+
+        /**
+         * getService by name
+         * @param name {String} service name
+         * @returns {Object} service
+         */
+        getService(name) {
+            return this._services[name];
+        }
+
+        /**
+         * getDisplayedServiceList
+         * @returns {Array.<Object>} visible layers
+         */
+        getDisplayedServiceList () {
             return this.services.filter(service => service.isDisplayed && services.layer);
         }
     }
