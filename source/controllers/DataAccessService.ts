6c4ea3d4178a87d6dda15ea9aa4c7bb044deb4eb
Maxim Gritsenko <maxim@gritsenko.biz>
2018-05-04 21:13:42 +0300

    DAS print method

--- source/controllers/DataAccessService.ts
+++ source/controllers/DataAccessService.ts
@@ -142,4 +142,73 @@ export class DataAccessService extends DataAccessBase {
         let { duration, solver, sourceServiceName, targetServiceName, resolutionK = null, uniteResults = false } = properties;
         return this.operation('isochroneByStorage', { duration, solver, sourceServiceName, targetServiceName, resolutionK, uniteResults });
     }
+
+    print(properties) {
+        const defaults = {
+            dpi: 96,
+            paperSize: {
+                width: 210,
+                height: 297
+            },
+            margin: {
+                left: 10,
+                top: 10,
+                right: 10,
+                bottom: 10
+            }
+        };
+
+        var description = <any>{
+            ServiceStateDefinition: [],
+            MapCenter: {
+                X: properties.position ? properties.position.x : properties.map.centerPoint.x,
+                Y: properties.position ? properties.position.y : properties.map.centerPoint.y
+            },
+            SpatialReference: properties.map.crs.toString(),
+            Dpi: properties.dpi || defaults.dpi,
+            Resolution: properties.resolution || properties.map.resolution,
+            PaperSize: {
+                Width: properties.paperSize && properties.paperSize.width || defaults.paperSize.width,
+                Height: properties.paperSize && properties.paperSize.height || defaults.paperSize.height
+            },
+            Margin: {
+                Left: properties.margin && properties.margin.left || defaults.margin.left,
+                Top: properties.margin && properties.margin.top || defaults.margin.top,
+                Right: properties.margin && properties.margin.right || defaults.margin.right,
+                Bottom: properties.margin && properties.margin.bottom || defaults.margin.bottom
+            },
+            PrintingTemplateName: properties.template.Name,
+            Parameters: []
+        };
+
+        for (var i = 0, len = properties.template.BindingGroups.length; i < len; i++) {
+            description.Parameters = description.Parameters.concat(properties.template.BindingGroups[i].Parameters);
+        }
+
+        var services = properties.services;
+        for (var i = 0, len = services.length; i < len; i++) {
+            let service = services[i];
+            description.ServiceStateDefinition.push({
+                UniqueName: service.name || service.id,
+                Opactiy: service.layer.opacity,
+                IsVisible: service.isDisplayed,
+                Title: service.name,
+                CustomParameters: {},
+                Layers: [{ LayerId: -1, LegendItemId: -1, Children: [] }]
+            });
+        }
+
+        description.Legend = {
+            LayerId: -1,
+            LegendItemId: -1,
+            Children: services.filter(x => x.hasLegend).map(x => {
+                return {
+                    Name: x.alias || x.name,
+                    ServiceFullName: x.name
+                };
+            })
+        };
+
+        return this.operation('print', {exportDefinition: description});
+    }
 }
