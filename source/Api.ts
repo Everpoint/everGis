78148ba5522e781a23ac32fa772bebb56f1ef455
Artem <attemka@gmail.com>
2018-07-16 14:47:45 +0300

    EP-30 fixed requests

--- source/Api.ts
+++ source/Api.ts
@@ -68,7 +68,7 @@ export class Api {
     }
 
     deleteProjects(data) {
-        return this._operation('projects/delete',{}, data)
+        return this._operation('projects/remove',{}, data)
     }
 
     loadProject(name) {
@@ -82,7 +82,7 @@ export class Api {
     }
 
     createProject(params) {
-        return this._operation('projects/create', {}, params)
+        return this._operation('projects/create', params, {})
             .then((response) => {
                 return response;
             })
