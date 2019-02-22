60192d9f01c40b3257a8099bd1c9da1c53e19a57
tanyaisinmybed <xvaultdwellerx@gmail.com>
2019-02-22 13:26:02 +0300

    0.4.18

--- source/everGis.ts
+++ source/everGis.ts
@@ -64,8 +64,8 @@ const sp = {
         ViewableController: ViewableController
     },
     ServiceSnappingProvider: ServiceSnappingProvider,
-    version: "0.4.1",
-    releaseDate: "11.07.2018"
+    version: "0.4.18",
+    releaseDate: "22.02.2019"
 };
 
 export default {sp};
